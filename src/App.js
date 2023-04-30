59dd4c3643e0212265584b7379fcb4126b3cf5dc
Sumit Savaliya <sumit@freshprints.com>
2023-04-30 22:47:08 +0530

    created signup login main components

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
+import { Route, Routes, Navigate } from 'react-router-dom';
+import Main from './components/Main';
+import Signup from './components/Signup';
+import Login from './components/Login';
+
 function App() {
+  const user = localStorage.getItem('token');
   return (
-    <div className='App'>
-      <h1>Hello World</h1>
-    </div>
+    <Routes>
+      {user && <Route path='/' exact element={<Main />} />}
+      <Route path='/signup' exact element={<Signup />} />
+      <Route path='/login' exact element={<Login />} />
+      <Route path='/' exact element={<Navigate replace to='/Login' />} />
+    </Routes>
   );
 }
 
