9860b91a80dc8fe4aa009841f91080db5fb8b1a7
Sumit Savaliya <sumit@freshprints.com>
2023-05-01 10:21:46 +0530

    added NASA Image Page on Main

--- src/components/Login/index.jsx
+++ src/components/Login/index.jsx
@@ -14,7 +14,7 @@ const Login = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      const url = 'http://localhost:8080/api/auth';
+      const url = `${process.env.REACT_APP_BACKEND_URL}/api/auth`;
       const { data: res } = await axios.post(url, data);
       localStorage.setItem('token', res.data);
       window.location = '/';
--- src/components/Main/index.jsx
+++ src/components/Main/index.jsx
@@ -1,6 +1,26 @@
+import { useState, useEffect } from 'react';
 import styles from './styles.module.css';
+import axios from 'axios';
 
 const Main = () => {
+  const [data, setData] = useState({});
+  const [error, setError] = useState('');
+
+  const getImage = async () => {
+    try {
+      const url = `${process.env.REACT_APP_BACKEND_URL}/api/image`;
+      const { data: res } = await axios.get(url);
+      console.log(res);
+      setData(res);
+    } catch (error) {
+      setError(error);
+    }
+  };
+
+  useEffect(() => {
+    getImage();
+  }, []);
+
   const handleLogout = () => {
     localStorage.removeItem('token');
     window.location.reload();
@@ -9,11 +29,20 @@ const Main = () => {
   return (
     <div className={styles.main_container}>
       <nav className={styles.navbar}>
-        <h1>fakebook</h1>
+        <h1>Nasa Image of the Day</h1>
         <button className={styles.white_btn} onClick={handleLogout}>
           Logout
         </button>
       </nav>
+      <div className={styles.main_container}>
+        {error ? (
+          <div className={styles.error_msg}>
+            <h1>Error fetching the image! Please try again later!!</h1>
+          </div>
+        ) : (
+          <img src={data.hdurl} alt='Please Wait!!' />
+        )}
+      </div>
     </div>
   );
 };
